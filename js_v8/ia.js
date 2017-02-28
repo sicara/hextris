fb176af77dfceb105ec160ce821913142e70c460
Adrien Lina <adrienlina@gmail.com>
2017-02-28 17:21:48 +0100

    Add lodash

--- js_v8/ia.js
+++ js_v8/ia.js
@@ -1,33 +1,33 @@
 function iaPlay() {
-  var bestRotation = 0;
-  var bestRotationScore = 0;
-  var mainHexAngle = ((MainHex.angle) / 60 + 60000 - 0.5) % 6;
+	var bestRotation = 0;
+	var bestRotationScore = 0;
+	var mainHexAngle = ((MainHex.angle) / 60 + 60000 - 0.5) % 6;
 
-  var lowestHeight = _.minBy(blocks, 'distFromHex').distFromHex;
-  var lowestBlocks = _.filter(blocks, function (block) {
-    return block.distFromHex < lowestHeight + 10;
-  });
+	var lowestHeight = _.minBy(blocks, 'distFromHex').distFromHex;
+	var lowestBlocks = _.filter(blocks, function (block) {
+		return block.distFromHex < lowestHeight + 10;
+	});
 
-  for (rotation = 0; rotation < 6; rotation++) {
-    var matches = [0,0,0,0,0,0];
-    lowestBlocks.forEach(function (block) {
-      var hexIndex = (MainHex.getLaneOfFallingBlock(block) + rotation) % 6;
-      if (_.last(MainHex.blocks[hexIndex])) {
-        if (_.last(MainHex.blocks[hexIndex]).color === block.color) {
-          console.log("ok");
-          matches[hexIndex] = matches[hexIndex] + 1;
-        }
-      }
-    });
-    var numberOfMatches = _.sum(matches);
-    if (numberOfMatches > bestRotationScore) {
-      console.log("Found better rotation: " + rotation + ", had " + numberOfMatches + " score.");
-      bestRotation = rotation;
-      bestRotationScore = numberOfMatches;
-    }
-  }
-  if (bestRotation !== 0 && MainHex.targetAngle === MainHex.angle) {
-    console.log("Found good rotation: " + bestRotation + ", had " + bestRotationScore + " score.");
-    MainHex.rotate(bestRotation);
-  }
+	for (rotation = 0; rotation < 6; rotation++) {
+		var matches = [0,0,0,0,0,0];
+		lowestBlocks.forEach(function (block) {
+			var hexIndex = (MainHex.getLaneOfFallingBlock(block) + rotation) % 6;
+			if (_.last(MainHex.blocks[hexIndex])) {
+				if (_.last(MainHex.blocks[hexIndex]).color === block.color) {
+					console.log("ok");
+					matches[hexIndex] = matches[hexIndex] + 1;
+				}
+			}
+		});
+		var numberOfMatches = _.sum(matches);
+		if (numberOfMatches > bestRotationScore) {
+			console.log("Found better rotation: " + rotation + ", had " + numberOfMatches + " score.");
+			bestRotation = rotation;
+			bestRotationScore = numberOfMatches;
+		}
+	}
+	if (bestRotation !== 0 && MainHex.targetAngle === MainHex.angle) {
+		console.log("Found good rotation: " + bestRotation + ", had " + bestRotationScore + " score.");
+		MainHex.rotate(bestRotation);
+	}
 }
